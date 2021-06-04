b2f111e46b0c62fd76032dca57af5c5956a365ae
KL <khoa@acuity.sg>
2021-06-04 17:13:50 +0700

    experimenting with the mongodb compressor

--- cryptofeed/backends/mongo.py
+++ cryptofeed/backends/mongo.py
@@ -14,8 +14,11 @@ from cryptofeed.backends.backend import (BackendBookCallback, BackendBookDeltaCa
 
 
 class MongoCallback:
-    def __init__(self, db, host='127.0.0.1', port=27017, key=None, numeric_type=str, **kwargs):
-        self.conn = motor.motor_asyncio.AsyncIOMotorClient(host, port)
+    def __init__(self, db, uri=None, host='127.0.0.1', port=27017, key=None, numeric_type=str, **kwargs):
+        if uri:
+            self.conn = motor.motor_asyncio.AsyncIOMotorClient(uri, **kwargs)
+        else:
+            self.conn = motor.motor_asyncio.AsyncIOMotorClient(host, port)
         self.db = self.conn[db]
         self.numeric_type = numeric_type
         self.collection = key if key else self.default_key
